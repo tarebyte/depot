9ccc7e3690534995b98c16aca9f67352a97cc4a5
Kyle Thompson <kyle@kylekthompson.com>
2015-02-16 00:04:19 -0500

    Find the user's cart through the session

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150216032521) do
+ActiveRecord::Schema.define(version: 20150216050127) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
